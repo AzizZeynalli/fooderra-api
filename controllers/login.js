95a2a0a0845808fcc06580dd5bbff3f2b93fc7ff
AzizZeynalli <a.zeynalli1@ufaz.az>
2024-01-07 02:49:59 +0400

    update

--- controllers/login.js
+++ controllers/login.js
@@ -4,21 +4,21 @@ const loginRouter = require('express').Router()
 const User = require('../models/user')
 
 loginRouter.post('/', async (request, response) => {
-    const { username, password } = request.body
+    const { email, password } = request.body
     
-    const user = await User.findOne({ username })
+    const user = await User.findOne({ email })
     const passwordCorrect = user === null 
       ? false 
       : await bcrypt.compare(password, user.passwordHash)
 
     if(!(user && passwordCorrect)){
         return response.status(401).json({
-            error: 'invalid username or password'
+            error: 'invalid email or password'
         })
     }
 
     const userForToken = {
-        username: user.username,
+        email: user.email,
         id: user._id
     }
 
